4c60fdd32d1e6157f1ae34d5a541bbe677bf7c88
Anekant <Anekantjainsagar@gmail.com>
2023-04-11 14:43:59 +0530

    ADded logo

--- src/Component/Navbar/index.js
+++ src/Component/Navbar/index.js
@@ -5,6 +5,10 @@ const Navbar = () => {
   const history = useNavigate();
   return (
     <div className="bg-blue-500 flex items-center justify-center mobile:py-3 md:py-1.5">
+      <img
+        src="/Ticketing App Logo.jpg"
+        style={{ width: "3%", marginRight: "0.5rem", borderRadius: "5rem" }}
+      />
       <p
         className="text-white font-bold text-2xl cursor-pointer"
         onClick={() => {
